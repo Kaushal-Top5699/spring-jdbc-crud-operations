f5c5a03caf961d47987049fb6bad86cc2d1c7aef
Kaushal Topinkatti B <kaushaltop@me.com>
2025-04-07 15:22:38 -0500

    More inserts

--- src/main/java/com/kaushal/Main.java
+++ src/main/java/com/kaushal/Main.java
@@ -8,7 +8,17 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         JdbcTemplate template = context.getBean(JdbcTemplate.class);
-        String insert = "INSERT INTO student VALUES(1, 'Kaushal', '101', '90')";
-        template.update(insert);
+//        String insert = "INSERT INTO student VALUES(1, 'Kaushal', '101', '90')";
+        int primaryKey = 3;
+        String name = "Suraj";
+        String rollNo = "103";
+        String marks = "92";
+        String insert = "INSERT INTO student VALUES(?, ?, ?, ?)";
+        int count = template.update(insert, primaryKey, name, rollNo, marks);
+        if (count > 0) {
+            System.out.println("Insertion success.");
+        } else {
+            System.out.println("Insertion fail.");
+        }
     }
 }
