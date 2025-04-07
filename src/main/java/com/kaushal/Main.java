6080d5d8653a782bf245c92471d6c0c8a30c3c02
Kaushal Topinkatti B <kaushaltop@me.com>
2025-04-07 15:31:44 -0500

    update & delete operations added.

--- src/main/java/com/kaushal/Main.java
+++ src/main/java/com/kaushal/Main.java
@@ -8,17 +8,49 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         JdbcTemplate template = context.getBean(JdbcTemplate.class);
+
+        // INSERT OPERATION START //
+
 //        String insert = "INSERT INTO student VALUES(1, 'Kaushal', '101', '90')";
-        int primaryKey = 3;
-        String name = "Suraj";
-        String rollNo = "103";
-        String marks = "92";
-        String insert = "INSERT INTO student VALUES(?, ?, ?, ?)";
-        int count = template.update(insert, primaryKey, name, rollNo, marks);
+//        int primaryKey = 4;
+//        String name = "Rohit";
+//        String rollNo = "104";
+//        String marks = "92";
+//        String insert = "INSERT INTO student VALUES(?, ?, ?, ?)";
+//        int count = template.update(insert, primaryKey, name, rollNo, marks);
+//        if (count > 0) {
+//            System.out.println("Insertion success.");
+//        } else {
+//            System.out.println("Insertion fail.");
+//        }
+
+        // INSERT OPERATION END //
+
+        // UPDATE OPERATION START //
+
+//        String marks = "96";
+//        int id = 1;
+//        String updateQuery = "UPDATE student SET marks=? WHERE id=?";
+//        int count = template.update(updateQuery, marks, id);
+//        if (count > 0) {
+//            System.out.println("Update Success.");
+//        } else {
+//            System.out.println("Update Fail.");
+//        }
+
+        // UPDATE OPERATION END //
+
+        // DELETE OPERATION START //
+
+        int id = 4;
+        String deleteQuery = "DELETE FROM student WHERE id=?";
+        int count = template.update(deleteQuery, id);
         if (count > 0) {
-            System.out.println("Insertion success.");
+            System.out.println("Delete Success.");
         } else {
-            System.out.println("Insertion fail.");
+            System.out.println("Delete Fail.");
         }
+
+        // DELETE OPERATION END //
     }
 }
